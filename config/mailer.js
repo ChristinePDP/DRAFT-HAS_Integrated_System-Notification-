7b8ed9d808da0eb95d4cb1294264e4accd8d8e4f
ChristinePDP <tinadepadua19@gmail.com>
2026-05-25 21:44:38 +0800

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -5,11 +5,14 @@ import 'dotenv/config';
 
 // GMAIL CONFIGURATION
 const transporter = nodemailer.createTransport({
-  service: process.env.NODEMAILER_SERVICE || 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // true para sa port 465 (SSL)
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds timeout para hindi mag-hang ng 2 mins kung sakaling i-block pa rin
 });
 
 export const sendEmail = async (to, subject, text) => {
